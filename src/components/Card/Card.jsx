8a6bd2581f36304f967e0366c5cbb433311a531c
Maxim <burning666@list.ru>
2024-11-24 20:01:48 +0700

    Create a project

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+const Card = () => {
+  return (
+    <>
+      <div className="Card">
+        <div className="Card__title">
+          <h4>Title</h4>
+        </div>
+        <div className="Card__date">
+          <h6>date</h6>
+        </div>
+        <div className="Card__par">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
