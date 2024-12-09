33ea0f6ebe7712f3ebb001838c3b7286564a071c
Maxim <burning666@list.ru>
2024-12-09 12:34:39 +0700

    Add posts layout

---
+++ src/utils/constant.js
@@ -0,0 +1,12 @@
+export const BASE_URL = "http://localhost:3000";
+
+export const APP_ROUTES = {
+    HOME: "/",
+    BLOG: "blog",
+    ABOUT: "/about",
+};
+
+export default API_ROUTES = {
+    ARTICLE_LIST_URL: "/api/articles",
+    ARTICLE_DETAI_URL: "/api/article",
+};
